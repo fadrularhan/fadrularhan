138cc3716a1741828ac06914fcb21123d212cbef
sysecret_ar <fadrularhan@users.noreply.github.com>
2021-03-15 16:40:09 +0700

    Add files via upload

---
+++ fadrularhan(tugas1).php
@@ -0,0 +1,23 @@
+<?php
+// array 
+$ikanku = ["Teri","Lele","Bawal","Tuna"];
+
+
+//menampilkan array 
+echo "Soal No.1";
+echo "<br>";
+
+echo "Ikan $ikanku[0] sering dijumpai di nasi kucing
+angkringan dan saya konsumsi pas tanggal tua.
+Kalo tanggal muda sering kuliner $ikanku[1], $ikanku[2],
+dan $ikanku[3]";
+
+echo "<br> <br> <br>";
+
+//mengubah format tanggal menjadi format indonesia
+echo "Soal No.2";
+echo "<br>";
+
+echo date('d-m-Y', strtotime('2017-03-14')); // Hasil 14-03-2017
+
+?>
